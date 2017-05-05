28ec84890d630cbdcaefd119108e85334f354f47
yh <yanghuancoder@163.com>
2017-05-05 09:54:21 +0800

    test

--- MyScanner.c
+++ MyScanner.c
@@ -6,6 +6,7 @@
  ************************************************************************/
 
 #include<stdio.h>
+#include<unistd.h>
 #include<stdlib.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
@@ -19,6 +20,7 @@ FILE *host_list;
 
 struct sockaddr_in *fillSockAddr(char *input);
 void connectAndScannerTestByRange(unsigned int min, unsigned int max);
+void connectAndScanner(unsigned int IP);
 
 int main(int argc, char *argv[]){
 	
@@ -27,7 +29,7 @@ int main(int argc, char *argv[]){
 	if(argc == 2){
 		//just scanner one IP's port
 		struct sockaddr_in *sa = fillSockAddr(argv[1]);
-		
+		connectAndScanner(sa->sin_addr.s_addr);
 	}else if(argc == 3){
 		//scanner IP from argv[1] to argv[2]
 		struct sockaddr_in *sa = fillSockAddr(argv[1]);
@@ -50,56 +52,61 @@ int main(int argc, char *argv[]){
 }
 
 struct sockaddr_in *fillSockAddr(char *input){
-	static struct sockaddr_in sa;
+	static struct sockaddr_in sock_in;
 	struct hostent *host;
-	memset(&sa, 0, sizeof(sa));
+	memset(&sock_in, 0, sizeof(sock_in));
 	
-	sa.sin_family = AF_INET;
+	sock_in.sin_family = AF_INET;
 
-	if(isdigit(input)){
-		sa.sin_addr.s_addr = inet_addr(input);
+	if(isdigit(input[0])){
+		sock_in.sin_addr.s_addr = inet_addr(input);
 	}else if((host = gethostbyname(input)) != 0){
-		strncpy((char *)&sa.sin_addr, (char *)host->h_addr, sizeof(sa.sin_addr));
+		strncpy((char *)&sock_in.sin_addr, (char *)host->h_addr, sizeof(sock_in.sin_addr));
 	}else{
 		fprintf(stdout, "invilad IP");
 		exit(1);
 	}
 
-	return &sa;
+	return &sock_in;
 }
 
 void connectAndScannerTestByRange(unsigned int min, unsigned int max){
-	struct sockaddr_in sa;
+	for(unsigned int i = min; i <= max; ++i){
+		connectAndScanner(i);
+	}
+}
+
+void connectAndScanner(unsigned int IP){
 	int net;
 	int error;
 	char *host;
+	
+	struct sockaddr_in sa;
+	memset(&sa, 0, sizeof(sa));
+	sa.sin_addr.s_addr = IP;
+	sa.sin_family = AF_INET;
 
-	if(net < 0){
-		perror("socket connect fail");
-		exit(3);
-	}
-
-	for(unsigned int i = min; i <= max; ++i){
-		memset(&sa, 0, sizeof(sa));
-		sa.sin_addr.s_addr = i;
-
-		for(int j = 1; j < 1024; ++j){
-
-			net = socket(AF_INET, SOCK_STREAM, 0);
-			sa.sin_port = htons(j);
-			error = connect(net, (struct sockaddr *)&sa, sizeof(sa));
-			if(error >= 0){
-				host = strcat(inet_ntoa(sa.sin_addr), ".txt");
-				host_list = fopen(host, "a+");
-				fprintf(host_list, "port: %d\n", j);
-				fclose(host_list);
-			}
+	for(int j = 1; j < 1024; ++j){
+		net = socket(AF_INET, SOCK_STREAM, 0);
+		if(net < 0){
+			perror("socket connect fail");
+			exit(3);
+		}
+		sa.sin_port = htons(j);
+		error = connect(net, (struct sockaddr *)&sa, sizeof(sa));
+		if(error == 0){
+			host = strcat(inet_ntoa(sa.sin_addr), ".txt");
+			host_list = fopen(host, "a+");
+			fprintf(host_list, "port: %d\n", j);
+			fclose(host_list);
 
 			if(shutdown(net, 2) < 0){
 				perror("shutdown");
 				exit(3);
 			}
 		}
+
 	}
 
+	close(net);
 }
